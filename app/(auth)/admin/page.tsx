1d8214f05fdf8e50938b6f7080b0d85dd8659409
Brendan Gibson <brendan@sienandco.com>
2025-02-05 17:06:42 -0700

    Use vercel's url 2

--- app/(auth)/admin/page.tsx
+++ app/(auth)/admin/page.tsx
@@ -1,7 +1,7 @@
 import AdminPage from "@/components/ui/admin/adminpage";
 
 export default async function () {
-  let data = await fetch(process.env.VERCEL_URL + "/api/leads");
+  let data = await fetch("https://" + process.env.VERCEL_URL + "/api/leads");
   let leads = await data.json();
   return <AdminPage leads={leads} />;
 }
